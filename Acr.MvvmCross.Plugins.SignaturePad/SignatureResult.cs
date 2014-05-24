c3fafd7f47aba52a6d67a477c2f72c8ff0c1f55a
Allan Ritchie <allan.ritchie@gmail.com>
2014-05-23 21:38:36 -0400

    Missed files and ignores

---
+++ Acr.MvvmCross.Plugins.SignaturePad/SignatureResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace Acr.MvvmCross.Plugins.SignaturePad {
+    
+    public class SignatureResult {
+
+        public Stream Stream { get; private set; }
+        public IEnumerable<DrawPoint> Points { get; private set; }
+        //public bool IsBlank
+
+
+        public SignatureResult(Stream stream, IEnumerable<DrawPoint> points) {
+            this.Stream = stream;
+            this.Points = points;
+        }
+    }
+}
